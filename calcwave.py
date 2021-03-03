130dac2d081119341a6c1b2f906bcc9edff5c31d
Justin <62445349+zenarcher007@users.noreply.github.com>
2021-03-03 01:35:53 -0600

    Update Licensing Info

--- calcwave.py
+++ calcwave.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 version = "1.0.0"
 
+
+# Copyright (C) 2021 by: Justin Douty (jdouty03 at gmail dot com)
+#This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Calcwave
+# Interactively generates and plays audio from mathematical formulas.
+  
 import sys
 from sys import platform
 import struct
