6c81192189a20d34644e01783958aef4f7b5f0bb
Justin <62445349+zenarcher007@users.noreply.github.com>
2021-03-03 18:58:46 -0600

    Add windows-curses dependency for Windows

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+import sys
+from setuptools import setup
+
+cursesModule = ""
+if sys.platform.startswith("win"):
+  cursesModule = "windows-curses"
+else:
+  cursesModule = "curses"
+
+setup(
+  name = 'calcwave',         # How you named your package folder (MyLib)
+  packages = ['calcwave'], #setuptools.find_packages(),   # Chose the same as "name"
+  version = '1.0.0b4',
+  license='gpl-3.0',
+  description = 'A simple cross-platform utility for generating and playing audio using a mathematical formula ',
+  author = 'Justin Douty',                   # Type in your name
+  url = 'https://github.com/zenarcher007/calcwave',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/zenarcher007/calcwave/archive/v1.0.0b4.tar.gz',
+  keywords = ['cross-platform', 'audio synthesis', 'terminal'],   # Keywords that define your package best
+  install_requires=[
+    'pyaudio',
+    'numpy',
+    'argparse',
+    'windows-curses ; platform_system=="Windows"',
+  ],
+  entry_points={
+    'console_scripts': [
+      'calcwave = calcwave:audiostudio'
+    ]
+  },
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: End Users/Desktop',      # Define that your audience are developers
+    'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+  #packages=setuptools.find_packages(),
+)
