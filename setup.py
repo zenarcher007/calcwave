395607cd4e3bb0f8e3d1d77035ccfb267637a09c
Justin <62445349+zenarcher007@users.noreply.github.com>
2021-03-04 13:53:57 -0600

    Take away install script in setup.py
    The installer is now in DependencyWizard and is run at runtime if needed.

--- setup.py
+++ setup.py
@@ -1,41 +1,17 @@
 import setuptools
 from setuptools import setup
-from setuptools.command.install import install
-import subprocess
-import os
-import sys
 
-# Help from https://stackoverflow.com/a/27953695 by 4Z4T4R
-# For Windows, try to install pyaudio through pipwin rather than from pip.
-class installScript(install):
-  def run(self):
-    if not sys.platform.startswith("win"):
-      install.run(self)
-      return
-    try:
-      sys.stderr.write("Attempting to install pyaudio through pipwin...")
-      subprocess.check_output(['python3', '-m', 'pip', 'install', 'pipwin'])
-      subprocess.check_output(['python3', '-m', 'pipwin', 'install', 'pyaudio'])
-      sys.stderr.write("Pyaudio was installed successfully!")
-    except:
-      sys.stderr.write("********* Unable to install pipwin and pyaudio automatically. Here is what I tried. Please install them by commands similar to these:\n")
-      sys.stderr.write("python3 -m pip install pipwin\n")
-      sys.stderr.write("python3 -m pipwin install pyaudio\n")
-    install.run(self)
-      
-
-setup(cmdclass={'install': installScript},
+setup(
   name = 'calcwave',         # How you named your package folder (MyLib)
   packages = ['calcwave'], #setuptools.find_packages(),   # Chose the same as "name"
-  version = '1.0.0b6',
+  version = '1.0.0b7',
   license='gpl-3.0',
   description = 'A simple cross-platform utility for generating and playing audio using a mathematical formula ',
   author = 'Justin Douty',                   # Type in your name
   url = 'https://github.com/zenarcher007/calcwave',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/zenarcher007/calcwave/archive/v1.0.0b6.tar.gz',
-  keywords = ['cross-platform', 'audio synthesis', 'terminal'],   # Keywords that define your package best
+  download_url = 'https://github.com/zenarcher007/calcwave/archive/v1.0.0b7.tar.gz',
+  keywords = ['cross-platform', 'audio synthesis', 'terminal', 'user-friendly'],   # Keywords that define your package best
   install_requires=[
-    'pyaudio',
     'numpy',
     'argparse',
     'windows-curses ; platform_system=="Windows"',
