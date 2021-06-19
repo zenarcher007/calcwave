07758dddb28870e22f4c181ccda55ae7406fc59f
Justin <jdouty03@icloud.com>
2021-06-18 21:40:17 -0500

    Fix enter key, implement auto-install on Linux

--- calcwave/dependencyWizard.py
+++ calcwave/dependencyWizard.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import cmd
+import shutil
 
 # I had too much trouble getting pyaudio to install as
 # a dependency to calcwave, due to it needing portaudio. I decided to
@@ -13,10 +14,17 @@ def runCmdWithFailMsg(command, failMsg):
   except subprocess.SubprocessError:
     sys.stdout.write(str(failMsg) + "\n")
     sys.exit(1)
+    
+# Checks if the specified command exists on your system.
+def commandExists(command):
+  if not isinstance(command, str):
+    raise TypeError(command)
+  return shutil.which(command) is not None
 
 # Mac install sequence
 def installPyaudio_mac():
-  sys.stderr.write("\nYou will need to install portaudio. Would you like me to install it for you? I will invoke these shell commands:\n\n")
+  sys.stderr.write("Detected Darwin operating system.\n")
+  sys.stderr.write("\nDependencyWizard: You will need to install the portaudio libraries, and the pyaudio Python module before continuing. Would you like me to install it for you? I will invoke these shell commands:\n\n")
   sys.stderr.write("brew install --HEAD portaudio\n")
   sys.stderr.write("python3 -m pip install pyaudio\n\n")
   text = input("Please type \"y\" or \"n\": ")
@@ -24,15 +32,16 @@ def installPyaudio_mac():
     sys.stderr.write("Quitting.\n")
     sys.exit(1)
   elif text == 'y':
-    runCmdWithFailMsg(['brew', 'install', '--HEAD', 'portaudio'], "DependencyWizard: Error invoking \"brew install --HEAD portaudio\". Please ensure that Homebrew is installed from https://brew.sh")
-    runCmdWithFailMsg(['python3', '-m', 'pip', 'install', 'pyaudio'], "DependencyWizard: Error invoking \"python3 -m pip install pyaudio\". Please ensure that portaudio is installed.")
+    runCmdWithFailMsg(['brew', 'install', '--HEAD', 'portaudio'], "DependencyWizard: Error invoking \"brew install --HEAD portaudio\". Please ensure that Homebrew is installed from https://brew.sh, or open an issue on github: https://github.com/zenarcher007/calcwave")
+    runCmdWithFailMsg(['python3', '-m', 'pip', 'install', 'pyaudio'], "DependencyWizard: Error invoking \"python3 -m pip install pyaudio\". Please ensure that portaudio is installed, or open an issue on github: https://github.com/zenarcher007/calcwave")
   else:
     sys.stderr.write("I'm not sure what you mean by \"" + text + "\". Quitting.\n")
     sys.exit(1)
     
 # Windows install sequence
 def installPyaudio_windows():
-  sys.stderr.write("\nWould you like me to install these for you? I will invoke these shell commands:\n\n")
+  sys.stderr.write("Detected Windows operating system.\n")
+  sys.stderr.write("\nDependencyWizard: You will need to install the pyaudio Python module before continuing. Would you like me to install these for you? I will invoke these shell commands:\n\n")
   sys.stderr.write("python3 -m pip install pipwin\n")
   sys.stderr.write("python3 -m pipwin install pyaudio\n\n")
   text = input("Please type \"y\" or \"n\": ")
@@ -40,33 +49,71 @@ def installPyaudio_windows():
     sys.stderr.write("Quitting.\n")
     sys.exit(1)
   elif text == 'y':
-    runCmdWithFailMsg(['python3', '-m', 'pip', 'install', 'pipwin'], "DependencyWizard: Error invoking \"python3 -m pip install pipwin\".")
-    runCmdWithFailMsg(['python3', '-m', 'pipwin', 'install', 'pyaudio'], "DependencyWizard: Error invoking \"python3 -m pip install pyaudio\".")
+    runCmdWithFailMsg(['python3', '-m', 'pip', 'install', 'pipwin'], "DependencyWizard: Error invoking \"python3 -m pip install pipwin\". If you believe that this is an error, please open an issue on github: https://github.com/zenarcher007/calcwave")
+    runCmdWithFailMsg(['python3', '-m', 'pipwin', 'install', 'pyaudio'], "DependencyWizard: Error invoking \"python3 -m pip install pyaudio\". If you believe that this is an error, please open an issue on github: https://github.com/zenarcher007/calcwave")
   else:
     sys.stderr.write("I'm not sure what you mean by \"" + text + "\". Quitting.\n")
     sys.exit(1)
     
 def installPyaudio_linux():
-  sys.stderr.write("\nWould you like me to install pyaudio for you? I will invoke these shell commands, but I may need help installing portaudio if needed:\n\n")
+  sys.stderr.write("Detected Linux operating system.\n")
+  # Find the correct install command for your package manager
+  sys.stderr.write("Checking your package manager...\n")
+  
+  # This is by no means a complete, or necessarily accurate list.
+  # I had no means to test all of these distros. However, feel free to
+  # open a pull request on Github about anything that needs to be added or changed.
+  installCmds = None
+  if commandExists('apt-get'): # Debian
+    installCmds = [['sudo', 'apt-get', 'update', '-y']]
+    installCmds.append(['sudo', 'apt-get', 'install', 'portaudio19-dev'])
+  elif commandExists('apk'): # Alpine
+    installCmds = [['sudo', 'apk', 'update']]
+    installCmds.append(['sudo', 'apk', 'add', 'portaudio-dev'])
+  elif commandExists('pacman'): # Arch
+    installCmds = [['sudo', 'pacman', '-Syu']]
+    installCmds.append(['sudo', 'pacman', '-S', 'portaudio'])
+  elif commandExists('eopkg'): # Solus
+    installCmds = [['sudo', 'eopkg', 'install', 'portaudio']]
+  elif commandExists('dnf'): # Fedora
+    installCmds = [['sudo', 'dnf', 'install', 'portaudio']]
+  elif commandExists('yum'): # Fedora and other RPM-based
+    installCmds = [['sudo', 'yum', 'install', 'portaudio']]
+  elif commandExists('zypper'): # OpenSUSE
+    installCmds = [['sudo', 'zypper', 'install', 'portaudio-devel']]
+  else:
+    sys.stderr.write("DependencyWizard: Failed to find a compatible package manager on your system.\n")
+    sys.stderr.write("You will need to install the portaudio libraries, as well as the pyaudio Python module before starting calcwave again.\n")
+    sys.stderr.write("This may be done with a command similar to \"<package manager> install portaudio\"\n")
+    sys.stderr.write("This may also be called portaudio19-dev, portaudio, or a similar name depending on your package manager.\n")
+    sys.stderr.write("You should then be able to install pyaudio, using a command similar to \"python3 -m pip install pyaudio\"\n")
+    sys.stderr.write("Please open an issue about this on github, describing your operating system and package manager types, or else feel free to contribute via a pull-request: https://github.com/zenarcher007/calcwave\n")
+    sys.stderr.write("Quitting...\n")
+    sys.exit(1)
+  
+  sys.stderr.write("\nDependencyWizard: You will need to install additional libraries before continuing. Would you like me to install them for you? I will invoke these shell commands. Your password may be necessary. Note that the automatic installer for Linux is in beta, and not all distros were tested.\n\n")
+  for cmd in installCmds:
+    sys.stderr.write(' '.join(cmd) + '\n')
   sys.stderr.write("python3 -m pip install pyaudio\n\n")
   text = input("Please type \"y\" or \"n\": ")
   if text == 'n':
     sys.stderr.write("Quitting.\n")
     sys.exit(1)
   elif text == 'y':
-    # There are many different package managers for linux. Either add support for each of them,
-    # or give up and ask you to install portaudio yourself.
-    runCmdWithFailMsg(['python3', '-m', 'pip', 'install', 'pyaudio'], "DependencyWizard: Error invoking \"python3 -m pip install pyaudio\". Please install \"portaudio19-dev\" through your package manager or manually, and run calcwave again.")
+    
+    for cmd in installCmds:
+      runCmdWithFailMsg(cmd, "DependencyWizard: Error invoking " + ' '.join(cmd) + ". Please install portaudio manually, and run calcwave again. Please open an issue about this on github: https://github.com/zenarcher007/calcwave")
+    runCmdWithFailMsg(['python3', '-m', 'pip', 'install', 'pyaudio'], "DependencyWizard: Error invoking \"python3 -m pip install pyaudio\". Please install \"portaudio\" through your package manager, and run calcwave again. Please open an issue about ths on github: https://github.com/zenarcher007/calcwave")
   else:
     sys.stderr.write("I'm not sure what you mean by \"" + text + "\". Quitting.\n")
     sys.exit(1)
     
     
 def main(argv = None):
-  sys.stderr.write("A missing module was detected. DependencyWizard will try to resolve missing dependencies for you.\n")
+  sys.stderr.write("A missing module was detected. DependencyWizard will try to resolve these missing dependencies for you.\n")
   try: # Is it pyaudio?
     import pyaudio
-    sys.stderr.write("Nothing seems to be wrong with pyaudio. I'm not sure what is wrong. Continuing.")
+    sys.stderr.write("DependencyWizard: Nothing seems to be wrong with pyaudio. I'm not sure what the problem is. Continuing anyways...\n")
   except ModuleNotFoundError:
     pform = sys.platform
     if "darwin" in pform:
@@ -77,8 +124,7 @@ def main(argv = None):
       installPyaudio_linux()
     else:
       sys.stderr.write("Sorry, DependencyWizard does not support your operating system. Please install pyaudio and/or portaudio manually.")
-  
-  sys.stderr.write("DependencyWizard completed sucessfully!\n")
+    sys.stderr.write("DependencyWizard completed sucessfully!\n")
       
   
 
