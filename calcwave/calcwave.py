aeeb874f43ac1c75849a3da9b2a219a08fa03104
Justin <62445349+zenarcher007@users.noreply.github.com>
2024-01-21 01:26:38 -0600

    Fix sound exception due to mathextensions import

--- calcwave/calcwave.py
+++ calcwave/calcwave.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-version = "1.6.1"
+version = "1.6.2"
 
 
 # Copyright (C) 2021 by: Justin Douty (jdouty03 at gmail dot com)
@@ -29,7 +29,7 @@ import threading
 import time
 import wave
 import gc
-import calcwave.mathextensions
+from calcwave import mathextensions
 import json
 import itertools
 
