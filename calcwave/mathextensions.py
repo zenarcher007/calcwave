Fix memory function call lookup, Derivative, add const()

calcwave/mathextensions.py
@@ -48,7 +48,7 @@ class Derivative:
         newY = 1
       elif newY < -1:
         newY = -1
-    self.prevY = newY
+    self.prevY = y
     return newY
 
   @staticmethod
@@ -71,18 +71,35 @@ class ExponentialMovingAverage:
   @staticmethod
   def __callname__():
     return "ema"
-  
+
+# A cache to avoid recomputing values that are known to not change.
+# This can greatly improve performance for user-defined values that would be
+# Recomputed at every iteration. This will update at every recompilation.
+class Constant:
+  def __init__(self):
+    self._initialized = False
+    self.cachedY = None
+
+  # Y should be any lambda that returns a value and takes no arguments
+  def evaluate(self, y):
+    if self._initialized == False:
+      self._initialized = True
+      self.cachedY = y()
+    return self.cachedY
+    
+  @staticmethod
+  def __callname__():
+    return "const"
+
 # A memistic convolution over the last number of values
 class Convolution:
   def __init__(self):
     self.length = 0
     self.history = deque(maxlen = self.length)
-    self.filter = None
 
   # Disable cache_filter if you will be using the scale parameter when filter values will be changing dynamically (likely uncommon)
   def evaluate(self, y, filter):
     filtlen = len(filter)
-
     if filtlen != self.length: # If the length of the filter is changed, recreate the deque with new maxlen
       oldh = self.history
       self.history = deque(maxlen = filtlen)
@@ -90,6 +107,7 @@ class Convolution:
     self.history.append(y)
     if filtlen != len(self.history):
       return y # There is not yet enough history to perform the convolution. Wait until there is.
+    #return np.dot(self.history, filter)
     return sum([a*b for a, b in zip(filter, self.history)])
  
   @staticmethod
@@ -100,7 +118,7 @@ class Convolution:
 # Any calls to functions mapped within their getFunctionTable() will be mapped to a unique
 # instance of that class.
 def getMemoryClasses():
-  return [Integral, Derivative, ExponentialMovingAverage, Convolution]
+  return [Integral, Derivative, ExponentialMovingAverage, Convolution, Constant]
 
 # During compilation, a mapping of functions available to the user
 def getFunctionTable():